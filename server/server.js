8403771cec6a4486c21500f4240a3b3a8765f709
Alireza Hamid <alirezahamid20@gmail.com>
2021-03-08 09:28:16 +0330

    setup es module in node and dotenv

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-const express = require("express")
-const app = express()
+import express from "express"
+import dotenv from "dotenv"
+import products from "./data/products.js"
 
-const products = require("./data/products")
+const app = express()
+dotenv.config()
 
 app.get("/", (req, res) => {
   res.send("API is running...")
@@ -16,4 +18,8 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log("Server running on port 5000"))
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
