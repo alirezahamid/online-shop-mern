f60eede4cb79afae940a5e486313af9454ce0277
Alireza Hamid <alirezahamid20@gmail.com>
2021-03-11 17:05:48 +0330

    implement action and reducer for login (redux)

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -7,17 +7,23 @@ import {
   productDetailsReducer,
 } from "./reducers/product.reducer"
 import { cartReducer } from "./reducers/cart.reducer"
+import { userLoginReducer } from "./reducers/user.reducer"
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 })
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : []
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
