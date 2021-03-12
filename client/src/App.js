e720269b413f7eef2eb1d0bb74c06236779a4c32
Alireza Hamid <alirezahamid20@gmail.com>
2021-03-12 13:05:35 +0330

    Implement Shipping address page

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import CartPage from "./pages/CartPage"
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
 import ProfilePage from "./pages/ProfilePage"
+import ShippingPage from "./pages/ShippingPage"
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
             <Route path="/login" component={LoginPage} exact />
             <Route path="/register" component={RegisterPage} exact />
             <Route path="/profile" component={ProfilePage} exact />
+            <Route path="/shipping" component={ShippingPage} exact />
             <Route path="/cart/:id?" component={CartPage} />
             <Route path="/" component={HomePage} exact />
           </Container>
