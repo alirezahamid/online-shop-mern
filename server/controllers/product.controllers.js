b9ad4a50459d1608e9aee719db40dabcb6ddf9bc
Alireza Hamid <alirezahamid20@gmail.com>
2021-03-16 13:29:33 +0330

    implement API for create and update product

--- server/controllers/product.controllers.js
+++ server/controllers/product.controllers.js
@@ -22,8 +22,60 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Create product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample Name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    category: "Sample category",
+    brand: "Sample brand",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  })
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+  // if (product) {
+})
+
+// @desc    Update product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error("Product Not found!")
+  }
+})
+
 // @desc    Delete product
-// @route   DELETE /api/product/:id
+// @route   DELETE /api/products/:id
 // @access  Private/Admin
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
@@ -36,4 +88,10 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById, deleteProduct }
+export {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+}
--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -4,17 +4,23 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from "../controllers/product.controllers.js"
 import { protect, admin } from "../middleware/auth.middleware.js"
 
-// @desc    Fetch all products
-// @route    GET /api/products
-// @access    public
-router.route("/").get(getProducts)
-
-// @desc    Fetch and Delete single product
-// @route    GET & DELETE /api/products/:id
+// @desc    Fetch and Create products
+// @route    GET & POST /api/products
 // @access    public and Private/Admin
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct)
+router.route("/").get(getProducts).post(protect, admin, createProduct)
+
+// @desc    Fetch and Delete and Update single product
+// @route    GET & DELETE & PUT /api/products/:id
+// @access    public and Private/Admin
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 export default router
