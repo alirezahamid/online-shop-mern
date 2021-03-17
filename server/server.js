1b382516b72ef000c92b247ab9790929fc5225be
Alireza Hamid <alirezahamid20@gmail.com>
2021-03-17 11:12:12 +0330

    implement upload in server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
@@ -7,6 +8,7 @@ import connectDB from "./config/db.js"
 import productRoutes from "./routes/products.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import orderRoutes from "./routes/order.routes.js"
+import uploadRoutes from "./routes/upload.routes.js"
 
 const app = express()
 dotenv.config()
@@ -19,10 +21,13 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
+app.use("/api/upload", uploadRoutes)
 
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
+const __dirname = path.resolve
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
 app.use(notFound)
 app.use(errorHandler)
